8bf58c2a1abfe2ea3902c7ab8f4f3dd77f182d9d
00peteromondi <00peteromondi@gmail.com>
2026-03-02 10:35:25 +0300

    Improved the homepage and lots of functionality.

--- baysoko/mpesa_settings.py
+++ baysoko/mpesa_settings.py
@@ -4,8 +4,8 @@ MPESA_CONSUMER_KEY='Musa9AXuj1qGVASOfJcklMUDIYAHik3fvTptAADE5c6ubNY7'
 MPESA_CONSUMER_SECRET='7e1lUudD6QZk5GOvYGQsIBSrzoGm141AQMYFG0kADpFR2DATHRSA6heQSZ6Amm3T'
 MPESA_BUSINESS_SHORTCODE = '174379'
 MPESA_PASSKEY = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
-#MPESA_CALLBACK_URL = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
-MPESA_CALLBACK_URL='https://bay-soko.onrender.com/mpesa-callback/'
+MPESA_CALLBACK_URL = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest'
+#MPESA_CALLBACK_URL='https://bay-soko.onrender.com/mpesa-callback/'
 MPESA_MAX_RETRIES=3
 MPESA_RETRY_INTERVAL=5
 
